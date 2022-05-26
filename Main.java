e19f3bcdb39411cf79c2a2fc201af5358c9146c8
Pieralessandro Garcia <103224509+pier1107@users.noreply.github.com>
2022-05-26 17:24:05 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Ejercicio5;
+
+public class Principal {
+ 
+    public static void main(String[] args) {
+       Raices ecuacion=new Raices(1,4,4); 
+       ecuacion.calcular(); 
+        
+    }
+     
+}
